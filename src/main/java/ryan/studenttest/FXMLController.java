e61738d5c3e3dca068a7b39c8bcb75c38bba1553
meghanr93 <meghanr93@BHSCUR149024SPW.kprdsb.schools.local>
2021-11-25 13:34:54 -0500

    exit button, imports and fixed hover

--- src/main/java/ryan/studenttest/FXMLController.java
+++ src/main/java/ryan/studenttest/FXMLController.java
@@ -12,31 +12,49 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class FXMLController implements Initializable {
     
     @FXML
     private Button btnList;
+    @FXML
+    private TextField txtFirstName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private TextField txtTest1;
+    @FXML
+    private TextField txtTest2;
+    @FXML
+    private TextField txtTest3;
+    @FXML
+    private TextField txtTest4;
+    @FXML
+    private Button btnAdd;
+    @FXML
+    private Button btnStudentAverage;
+    @FXML
+    private Button btnCourseAverage;
+    @FXML
+    private Button btnExit;
    
-    boolean hovered=false;
     double buttonSetHeight=0;
     double buttonSetWidth=0;
     
     @FXML
     void btnHover(MouseEvent event) {
         /* Causes Buttons to expand when hovered over. */
-        hovered=true;
         Button button = (Button) event.getSource();
         buttonSetHeight=button.getPrefHeight();
         buttonSetWidth=button.getPrefWidth();
         button.setPrefHeight(buttonSetHeight*1.1);
         button.setPrefWidth(buttonSetWidth*1.1);
         button.setTranslateX((buttonSetWidth-buttonSetWidth*1.1)/2);
-        button.setTranslateY((buttonSetWidth-buttonSetWidth*1.1)/2);
+        button.setTranslateY((buttonSetHeight-buttonSetHeight*1.1)/2);
         button.toFront();     
-    }
-    
+    }   
     @FXML
     void btnUnhover(MouseEvent event) {
         /* Causes expanded Buttons to shrink back to original size when not hovered over. */
@@ -51,6 +69,26 @@ public class FXMLController implements Initializable {
     @FXML
     void btnListClick(ActionEvent event) {
 
+    }
+    
+    @FXML
+    void btnAddClick(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void btnStudentAverageClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCourseAverageClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnExitClick(ActionEvent event) {
+        System.exit(0);
     }
     
     @Override
